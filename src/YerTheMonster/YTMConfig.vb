ad6532feba2b796c9de2cf9e297683b296579d35
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-17 10:37:39 -0500

    config

--- src/YerTheMonster/YTMConfig.vb
+++ src/YerTheMonster/YTMConfig.vb
@@ -1,3 +1,6 @@
-﻿Public Class YTMConfig
-
+﻿Friend Class YTMConfig
+    Public Property WindowWidth As Integer
+    Public Property WindowHeight As Integer
+    Public Property FullScreen As Boolean
+    Public Property SfxVolume As Single
 End Class
