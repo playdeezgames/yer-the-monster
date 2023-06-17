c19f460ff4622560314fc255cb5784467c4ee0e5
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-17 04:21:18 -0500

    shipped it!

---
+++ shippit.sh
@@ -0,0 +1,8 @@
+dotnet publish ./src/YerTheMonster/YerTheMonster.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./src/YerTheMonster/YerTheMonster.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./src/YerTheMonster/YerTheMonster.vbproj -o ./pub-mac -c Release --sc -r osx-x64
+butler push pub-windows thegrumpygamedev/a-game-in-vbnet-about-yer-the-monster-and-starting-with-nothing:windows
+butler push pub-linux thegrumpygamedev/a-game-in-vbnet-about-yer-the-monster-and-starting-with-nothing:linux
+butler push pub-mac thegrumpygamedev/a-game-in-vbnet-about-yer-the-monster-and-starting-with-nothing:mac
+git add -A
+git commit -m "shipped it!"
---
+++ src/YerTheMonster/Program.vb
@@ -0,0 +1,8 @@
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Yer, The Monster!")
+        While Not Console.KeyAvailable
+            'nothing!
+        End While
+    End Sub
+End Module
