a9760be7fe7b71d35eda1f5050ec59f188341564
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-17 12:05:33 -0500

    main menu is now base menu

--- src/YTM.Application/MainMenuState.vb
+++ src/YTM.Application/MainMenuState.vb
@@ -1,42 +1,18 @@
 ﻿Friend Class MainMenuState
-    Inherits BaseGameState(Of Hue, Command, Sfx, GameState)
-    Private ReadOnly MenuItems() As String = {EmbarkText, LoadText, OptionsText, AboutText, QuitText}
-    Private MenuItemIndex As Integer = 0
+    Inherits BaseMenuState
     Public Sub New(parent As IGameController(Of Hue, Command, Sfx), setState As Action(Of GameState?, Boolean))
-        MyBase.New(parent, setState)
+        MyBase.New(
+            parent,
+            setState, MainMenuTitle, {EmbarkText, LoadText, OptionsText, AboutText, QuitText}, QuitText)
     End Sub
-    Public Overrides Sub HandleCommand(command As Command)
-        Select Case command
-            Case Command.Up
-                MenuItemIndex = (MenuItemIndex + MenuItems.Length - 1) Mod MenuItems.Length
-            Case Command.Down
-                MenuItemIndex = (MenuItemIndex + 1) Mod MenuItems.Length
-            Case Command.A
-                HandleMenuItem(MenuItems(MenuItemIndex))
-            Case Command.B
-                HandleMenuItem(QuitText)
-        End Select
-    End Sub
-
-    Private Sub HandleMenuItem(menuItem As String)
+    Protected Overrides Sub HandleMenuItem(menuItem As String)
         Select Case menuItem
             Case QuitText
                 SetState(GameState.ConfirmQuit)
             Case OptionsText
                 SetState(GameState.Options)
+            Case Else
+                Throw New NotImplementedException()
         End Select
-    End Sub
-
-    Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
-        displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
-        Dim font = Fonts.GetFont(GameFont.Font5x7)
-        With font
-            .WriteText(displayBuffer, (0, 0), MainMenuTitle, Hue.White)
-            Dim y = font.Height
-            For index = 0 To MenuItems.Length - 1
-                .WriteText(displayBuffer, (0, y), MenuItems(index), If(index = MenuItemIndex, Hue.Magenta, Hue.Cyan))
-                y += font.Height
-            Next
-        End With
     End Sub
 End Class
