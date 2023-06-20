a5c20f7a563be3a534a70727ca381255bcad5cb2
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-20 05:52:47 -0500

    inventory is now a picker

--- src/YTM.Application/States/InventoryState.vb
+++ src/YTM.Application/States/InventoryState.vb
@@ -1,35 +1,22 @@
 ﻿Friend Class InventoryState
-    Inherits BaseGameState(Of Hue, Command, Sfx, GameState)
-    Private _menuItems As New List(Of (String, String))
-    Private _menuItemIndex As Integer
+    Inherits BasePickerState
     Public Sub New(parent As IGameController(Of Hue, Command, Sfx), setState As Action(Of GameState?, Boolean))
-        MyBase.New(parent, setState)
+        MyBase.New(parent, setState, "Yer's Inventory", "Space/(A) - Item Details | Esc/(B) - Go Back", GameState.Neutral)
     End Sub
-    Public Overrides Sub HandleCommand(command As Command)
-        Select Case command
-            Case Command.B
+
+    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
+        Select Case value.Item2
+            Case NeverMindText
                 SetState(GameState.Neutral)
-            Case Command.A
         End Select
     End Sub
-    Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
-        displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
-        Dim font = Fonts.GetFont(GameFont.Font5x7)
-        Dim y = ViewHeight \ 2 - font.Height \ 2 - _menuItemIndex * font.Height
-        Dim index = 0
-        For Each menuItem In _menuItems
-            Dim x = ViewWidth \ 2 - font.TextWidth(menuItem.Item1) \ 2
-            Dim h = If(index = _menuItemIndex, Hue.Magenta, Hue.Cyan)
-            font.WriteText(displayBuffer, (x, y), menuItem.Item1, h)
-            index += 1
-            y += font.Height
-        Next
-        ShowStatusBar(displayBuffer, font, "Space/(A) - Item Details | Esc/(B) - Go Back", Hue.Black, Hue.White)
-    End Sub
-    Public Overrides Sub OnStart()
-        MyBase.OnStart()
-        Dim avatar = World.Avatar
-        _menuItems = avatar.Items.GroupBy(Function(x) x.Name).Select(Function(x) ($"{x.Key}(x{x.Count})", x.Key)).ToList
-        _menuItemIndex = 0
-    End Sub
+
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
+        Dim result As New List(Of (String, String)) From
+            {
+                (NeverMindText, NeverMindText)
+            }
+        result.AddRange(World.Avatar.Items.GroupBy(Function(x) x.Name).Select(Function(x) ($"{x.Key}(x{x.Count})", x.Key)))
+        Return result
+    End Function
 End Class
