52261cfa063bcd267f332cd5676c3e9142455227
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-20 19:20:20 -0500

    satiety decrease and health decrease.

--- src/YTM.Initializer/Extensions/CharacterExtensions.vb
+++ src/YTM.Initializer/Extensions/CharacterExtensions.vb
@@ -3,16 +3,35 @@
 Public Module CharacterExtensions
     <Extension>
     Public Sub Move(character As ICharacter, direction As String)
+        character.ApplyHunger(1)
         character.Location = character.Location.Route(direction).Destination
+    End Sub
+    <Extension>
+    Friend Sub ApplyHunger(character As ICharacter, hunger As Integer)
+        If character.Satiety >= hunger Then
+            character.SetSatiety(character.Satiety - hunger)
+            Return
+        End If
+        hunger -= character.Satiety
+        character.SetSatiety(0)
+        character.SetHealth(character.Health - hunger)
     End Sub
     <Extension>
     Public Function Health(character As ICharacter) As Integer
         Return character.Statistic(StatisticTypes.Health)
     End Function
     <Extension>
+    Friend Sub SetHealth(character As ICharacter, health As Integer)
+        character.Statistic(StatisticTypes.Health) = Math.Clamp(health, 0, character.MaximumHealth)
+    End Sub
+    <Extension>
     Public Function MaximumHealth(character As ICharacter) As Integer
         Return character.Statistic(StatisticTypes.MaximumHealth)
     End Function
+    <Extension>
+    Friend Sub SetSatiety(character As ICharacter, satiety As Integer)
+        character.Statistic(StatisticTypes.Satiety) = Math.Clamp(satiety, 0, character.MaximumSatiety)
+    End Sub
     <Extension>
     Public Function Satiety(character As ICharacter) As Integer
         Return character.Statistic(StatisticTypes.Satiety)
