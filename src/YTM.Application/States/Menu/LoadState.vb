d5b7adb1d820d96360d92133a51d5a4317996796
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-24 04:36:09 -0500

    load screen

--- src/YTM.Application/States/Menu/LoadState.vb
+++ src/YTM.Application/States/Menu/LoadState.vb
@@ -14,12 +14,17 @@
             },
             GoBackText)
     End Sub
+    Private ReadOnly Property SlotIndex As Integer
+        Get
+            Return MenuItemIndex + 1
+        End Get
+    End Property
     Protected Overrides Sub HandleMenuItem(menuItem As String)
         Select Case menuItem
             Case GoBackText
                 SetState(GameState.MainMenu)
             Case Else
-                If DoesSaveExist(MenuItemIndex) Then
+                If DoesSaveExist(SlotIndex) Then
                     Context.LoadFromSlot(MenuItemIndex)
                     SetState(GameState.Neutral)
                 Else
@@ -30,14 +35,11 @@
     Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
         MyBase.Render(displayBuffer)
         Dim font = Fonts.GetFont(GameFont.Font5x7)
-        If MenuItemIndex > 0 Then
-            If DoesSaveExist(MenuItemIndex) Then
-                ShowStatusBar(displayBuffer, font, SaveSlotExistsText, Hue.Black, Hue.LightGray)
-            Else
-                ShowStatusBar(displayBuffer, font, SaveSlotDoesNotExistText, Hue.Black, Hue.Red)
-            End If
+        If DoesSaveExist(SlotIndex) Then
+            ShowHeader(displayBuffer, font, SaveSlotExistsText, Hue.Black, Hue.LightGray)
         Else
-            ShowStatusBar(displayBuffer, font, "On second thought...", Hue.Black, Hue.LightGray)
+            ShowHeader(displayBuffer, font, SaveSlotDoesNotExistText, Hue.Black, Hue.Red)
         End If
+        ShowStatusBar(displayBuffer, font, ControlsText("Load", "Cancel"), Hue.Black, Hue.LightGray)
     End Sub
 End Class
