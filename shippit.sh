99b67f817c19fda0a61e8dcc7597a9561afd45c3
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-26 05:18:15 -0500

    remove pub folders first

--- shippit.sh
+++ shippit.sh
@@ -1,3 +1,6 @@
+rm -rf ./pub-linux
+rm -rf ./pub-windows
+rm -rf ./pub-mac
 dotnet publish ./src/YerTheMonster/YerTheMonster.vbproj -o ./pub-linux -c Release --sc -r linux-x64
 dotnet publish ./src/YerTheMonster/YerTheMonster.vbproj -o ./pub-windows -c Release --sc -r win-x64
 dotnet publish ./src/YerTheMonster/YerTheMonster.vbproj -o ./pub-mac -c Release --sc -r osx-x64
