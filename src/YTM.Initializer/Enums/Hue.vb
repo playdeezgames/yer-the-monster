f76f133cdafd02c0907dbf77fea8baaa44ac51aa
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-27 10:21:45 -0500

    change the hue

--- src/YTM.Initializer/Enums/Hue.vb
+++ src/YTM.Initializer/Enums/Hue.vb
@@ -1,18 +1,18 @@
 ﻿Public Module Hue
     Public Const Black = 0
-    Public Const Cyan = 1
-    Public Const Purple = 2
-    Public Const White = 3
-    Public Const DarkGray = 4
-    Public Const Red = 5
-    Public Const Blue = 6
-    Public Const Green = 7
-    Public Const Brown = 8
-    Public Const LightGray = 9
+    Public Const Blue = 1
+    Public Const Green = 2
+    Public Const Cyan = 3
+    Public Const Red = 4
+    Public Const Purple = 5
+    Public Const Brown = 6
+    Public Const LightGray = 7
+    Public Const DarkGray = 8
+    Public Const LightBlue = 9
     Public Const LightGreen = 10
-    Public Const LightBlue = 11
-    Public Const Orange = 12
-    Public Const Yellow = 13
-    Public Const Tan = 14
-    Public Const Pink = 15
+    Public Const Orange = 11
+    Public Const Pink = 12
+    Public Const Tan = 13
+    Public Const Yellow = 14
+    Public Const White = 15
 End Module
