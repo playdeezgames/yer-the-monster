9d2045697e69ef0eebc532d78b8a66a32a7342cc
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-27 16:18:18 -0500

    gamefont becomes string

--- src/YTM.Application/Enums/GameFont.vb
+++ src/YTM.Application/Enums/GameFont.vb
@@ -1,7 +1,7 @@
-﻿Public Enum GameFont
-    Font3x5
-    Font4x6
-    Font5x7
-    Font8x8
-    YTM
-End Enum
+﻿Public Module GameFont
+    Public Const Font3x5 = "Font3x5"
+    Public Const Font4x6 = "Font4x6"
+    Public Const Font5x7 = "Font5x7"
+    Public Const Font8x8 = "Font8x8"
+    Public Const YTM = "YTM"
+End Module
--- src/YTM.Application/Utility/Fonts.vb
+++ src/YTM.Application/Utility/Fonts.vb
@@ -1,6 +1,6 @@
 ﻿Friend Module Fonts
-    Private ReadOnly fontFilenames As IReadOnlyDictionary(Of GameFont, String) =
-        New Dictionary(Of GameFont, String) From
+    Private ReadOnly fontFilenames As IReadOnlyDictionary(Of String, String) =
+        New Dictionary(Of String, String) From
         {
             {GameFont.Font3x5, "Content/CyFont3x5.json"},
             {GameFont.Font4x6, "Content/CyFont4x6.json"},
@@ -8,13 +8,13 @@
             {GameFont.Font8x8, "Content/CyFont8x8.json"},
             {GameFont.YTM, "Content/ytm.json"}
         }
-    Private fonts As New Dictionary(Of GameFont, Font)
+    Private fonts As New Dictionary(Of String, Font)
     Friend Sub Load()
         For Each entry In fontFilenames
             fonts(entry.Key) = New Font(JsonSerializer.Deserialize(Of FontData)(File.ReadAllText(entry.Value)))
         Next
     End Sub
-    Friend Function GetFont(gameFont As GameFont) As Font
+    Friend Function GetFont(gameFont As String) As Font
         Return fonts(gameFont)
     End Function
 End Module
