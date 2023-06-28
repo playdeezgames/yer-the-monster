b4b689d891cda5574ebb212e86f47a3c076d6876
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-28 05:07:19 -0500

    pickerized action menu

--- src/YTM.Application/States/Menu/ActionMenuState.vb
+++ src/YTM.Application/States/Menu/ActionMenuState.vb
@@ -1,33 +1,23 @@
 ﻿Friend Class ActionMenuState
-    Inherits BaseMenuState
+    Inherits BasePickerState
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), fontSource As IFontSource)
-        MyBase.New(
-            parent,
-            setState,
-            fontSource,
-            ActionMenuTitle,
-            {
-                InventoryText,
-                PickUpItemText
-            },
-            GoBackText)
+        MyBase.New(parent, setState, fontSource, "Actions", ControlsText("Select", "Cancel"), GameState.Neutral)
     End Sub
-    Protected Overrides Sub HandleMenuItem(menuItem As String)
-        Select Case menuItem
-            Case GoBackText
-                SetState(GameState.Neutral)
+
+    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
+        Select Case value.Item2
             Case InventoryText
                 SetState(GameState.Inventory)
             Case Else
                 SetState(GameState.PickUpItem)
         End Select
     End Sub
-    Public Overrides Sub Render(displayBuffer As IPixelSink)
-        MyBase.Render(displayBuffer)
-        Dim font = FontSource.GetFont(GameFont.Font5x7)
-        Dim avatar = World.Avatar
-        With font
-            ShowStatusBar(displayBuffer, font, "Space/(A) - Select | Esc/(B) - Go back", Hue.Black, Hue.LightGray)
-        End With
-    End Sub
+
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
+        Return New List(Of (String, String)) From
+            {
+                (InventoryText, InventoryText),
+                (PickUpItemText, PickUpItemText)
+            }
+    End Function
 End Class
