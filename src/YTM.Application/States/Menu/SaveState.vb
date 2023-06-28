32d71b5d41e5ac9011a13083848952782307cb1b
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-28 05:17:57 -0500

    formattingh

--- src/YTM.Application/States/Menu/SaveState.vb
+++ src/YTM.Application/States/Menu/SaveState.vb
@@ -3,13 +3,11 @@
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), fontSource As IFontSource)
         MyBase.New(parent, setState, fontSource, "Save Game", ControlsText("Select", "Cancel"), GameState.GameMenu)
     End Sub
-
     Protected Overrides Sub OnActivateMenuItem(value As (String, String))
         Dim slotIndex = CInt(value.Item2)
         Context.SaveToSlot(slotIndex)
         SetState(GameState.GameMenu)
     End Sub
-
     Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
         Dim result As New List(Of (String, String))
         For slotIndex = 1 To 5
