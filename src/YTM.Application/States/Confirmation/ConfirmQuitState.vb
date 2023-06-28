a7ed829a51d6f0fceca9321c7e10c4c186253f94
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-28 05:47:16 -0500

    confirm quit is pickerized

--- src/YTM.Application/States/Confirmation/ConfirmQuitState.vb
+++ src/YTM.Application/States/Confirmation/ConfirmQuitState.vb
@@ -1,38 +1,21 @@
 ﻿Friend Class ConfirmQuitState
-    Inherits BaseGameState
-    Private Confirmation As Boolean = False
+    Inherits BasePickerState
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), fontSource As IFontSource)
-        MyBase.New(parent, setState, fontSource)
+        MyBase.New(parent, setState, fontSource, "Are you sure you want to quit?", ControlsText("Select", "Cancel"), GameState.MainMenu)
     End Sub
-
-    Public Overrides Sub HandleCommand(cmd As String)
-        Select Case cmd
-            Case Command.Up, Command.Down
-                Confirmation = Not Confirmation
-            Case Command.A
-                If Confirmation Then
-                    PopState()
-                Else
-                    SetState(GameState.MainMenu)
-                End If
-            Case Command.B
-                Confirmation = False
+    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
+        Select Case value.Item2
+            Case NoText
                 SetState(GameState.MainMenu)
+            Case YesText
+                PopState()
         End Select
     End Sub
-
-    Public Overrides Sub Render(displayBuffer As IPixelSink)
-        displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
-        Dim font = FontSource.GetFont(GameFont.Font5x7)
-        With font
-            .WriteText(displayBuffer, (0, 0), ConfirmQuitTitle, Hue.Red)
-            .WriteText(displayBuffer, (0, font.Height), NoText, If(Confirmation, Hue.Blue, Hue.LightBlue))
-            .WriteText(displayBuffer, (0, font.Height * 2), YesText, If(Confirmation, Hue.LightBlue, Hue.Blue))
-        End With
-        If Confirmation Then
-            ShowStatusBar(displayBuffer, font, "Please don't go!", Hue.Black, Hue.Red)
-        Else
-            ShowStatusBar(displayBuffer, font, "You'll stay?", Hue.Black, Hue.LightGray)
-        End If
-    End Sub
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
+        Return New List(Of (String, String)) From
+            {
+                (NoText, NoText),
+                (YesText, YesText)
+            }
+    End Function
 End Class
