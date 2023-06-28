4e5b86bff68d0195ab0473b5f76d6b67b2fb6395
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-28 05:41:33 -0500

    window size is pickerized.

--- src/YTM.Application/States/Menu/WindowSizeState.vb
+++ src/YTM.Application/States/Menu/WindowSizeState.vb
@@ -1,45 +1,34 @@
 ﻿Friend Class WindowSizeState
-    Inherits BaseMenuState
-    Private _saveConfig As Action
+    Inherits BasePickerState
+    Private ReadOnly _saveConfig As Action
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), fontSource As IFontSource, saveConfig As Action)
-        MyBase.New(
-            parent,
-            setState,
-            fontSource,
-            WindowSizeTitle,
-            {
-                Scale1Text,
-                Scale2Text,
-                Scale3Text,
-                Scale4Text,
-                Scale5Text,
-                Scale6Text,
-                Scale7Text,
-                Scale8Text,
-                Scale9Text,
-                Scale10Text,
-                Scale11Text,
-                Scale12Text
-            },
-            GoBackText)
+        MyBase.New(parent, setState, fontSource, "<placeholder>", ControlsText("Select", "Cancel"), GameState.Options)
         _saveConfig = saveConfig
     End Sub
-    Protected Overrides Sub HandleMenuItem(menuItem As String)
-        Select Case menuItem
-            Case GoBackText
-                SetState(GameState.Options)
-            Case Else
-                Dim tokens = menuItem.Split("x"c)
-                Dim width = CInt(tokens(0))
-                Dim height = CInt(tokens(1))
-                Parent.Size = (width, height)
-                _saveConfig()
-        End Select
+    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
+        Dim tokens = value.Item2.Split("x"c)
+        Dim width = CInt(tokens(0))
+        Dim height = CInt(tokens(1))
+        Parent.Size = (width, height)
+        _saveConfig()
+        HeaderText = $"Current Size: {Size.Item1}x{Size.Item2}"
     End Sub
-    Public Overrides Sub Render(displayBuffer As IPixelSink)
-        MyBase.Render(displayBuffer)
-        Dim font = FontSource.GetFont(GameFont.Font5x7)
-        Title = $"Window Size: {Parent.Size.Item1}x{Parent.Size.Item2}"
-        ShowStatusBar(displayBuffer, font, ControlsText("Choose", "Cancel"), Hue.Black, Hue.LightGray)
-    End Sub
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
+        HeaderText = $"Current Size: {Size.Item1}x{Size.Item2}"
+        Return New List(Of (String, String)) From
+            {
+                (Scale1Text, Scale1Text),
+                (Scale2Text, Scale2Text),
+                (Scale3Text, Scale3Text),
+                (Scale4Text, Scale4Text),
+                (Scale5Text, Scale5Text),
+                (Scale6Text, Scale6Text),
+                (Scale7Text, Scale7Text),
+                (Scale8Text, Scale8Text),
+                (Scale9Text, Scale9Text),
+                (Scale10Text, Scale10Text),
+                (Scale11Text, Scale11Text),
+                (Scale12Text, Scale12Text)
+            }
+    End Function
 End Class
